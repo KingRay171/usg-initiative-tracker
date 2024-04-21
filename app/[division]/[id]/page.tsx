2e3cdd42d9781e84308b6a6bc871043ad6dffec5
Ray Ikome <raycikome@gmail.com>
2024-04-21 10:26:37 -0400

    suspense for nav + inits

--- app/[division]/[id]/page.tsx
+++ app/[division]/[id]/page.tsx
@@ -2,46 +2,53 @@ import EditDelete from "@/app/components/editdelete"
 import { InitType } from "@prisma/client";
 import { validateRequest } from "@/auth"; 
 import { getAllLeaders, getInitFromId } from "@/app/queries";
+import { Suspense } from "react";
+
+const handleInitExists = async ({params}:{params: {division: string, id: string}}, divisionEnum: InitType) => {
+    const session = await validateRequest()
+    const init = await getInitFromId(params.id)
+    if(!init || init.type !== divisionEnum) return (
+        <>
+        <div>This initiative does not exist</div>
+        <p>The URL references an initiative that could not be found. Either it was recently deleted or a mistake was made.</p>
+        </>
+      )
+      const leaders = await getAllLeaders()
+      return (
+          <div className="dark:bg-[#1B1D1E]">
+          <div className="border border-2 rounded-2xl mx-8 my-8 flex flex-col px-16 py-4 space-y-4">
+          <h1 className="text-6xl">{init.name}</h1>
+          <div className="flex space-x-4">
+              <h3 className="text-3xl">Description:</h3>
+              <h3 className="text-3xl">{init.description}</h3>
+          </div>
+          <div className="flex space-x-4">
+              <h3 className="text-3xl">Status:</h3>
+              <h3 className="text-3xl">{init.status}</h3>
+          </div>
+          <div className="flex flex-col space-y-2">
+              <h3 className="text-3xl">Initiative Leaders:</h3>
+              {init.leaders.map(e => (
+                  <h3 key={e.id} className="text-3xl">{e.name} ({e.email})</h3>
+              ))}
+          </div>
+              
+          {session.session && <EditDelete params={init} leaders={leaders} />}
+          </div>
+          </div>
+      )
+}
 
 export default async function Page({params}:{params: {division: string, id: string}}){
 
-    const session = await validateRequest()
+    
     const divisionEnum = InitType[params.division.toUpperCase() as keyof typeof InitType]
     if (!divisionEnum){
       return <></>
     }
-    const init = await getInitFromId(params.id)
-    
-    if(!init || init.type !== divisionEnum) return (
-      <>
-      <div>This initiative does not exist</div>
-      <p>The URL references an initiative that could not be found. Either it was recently deleted or a mistake was made.</p>
-      </>
-    )
-    const leaders = await getAllLeaders()
+
     return (
-        <div className="dark:bg-[#1B1D1E]">
-        <div className="border border-2 rounded-2xl mx-8 my-8 flex flex-col px-16 py-4 space-y-4">
-        <h1 className="text-6xl">{init.name}</h1>
-        <div className="flex space-x-4">
-            <h3 className="text-3xl">Description:</h3>
-            <h3 className="text-3xl">{init.description}</h3>
-        </div>
-        <div className="flex space-x-4">
-            <h3 className="text-3xl">Status:</h3>
-            <h3 className="text-3xl">{init.status}</h3>
-        </div>
-        <div className="flex flex-col space-y-2">
-            <h3 className="text-3xl">Initiative Leaders:</h3>
-            {init.leaders.map(e => (
-                <h3 key={e.id} className="text-3xl">{e.name} ({e.email})</h3>
-            ))}
-        </div>
-            
-        {session.session && <EditDelete params={init} leaders={leaders} />}
-        </div>
-            
-        </div>
+        <Suspense>{handleInitExists({params}, divisionEnum)}</Suspense>
     )
     
 }
--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -5,6 +5,7 @@ import NavRight from "./navright";
 import NavBurger from "./navburger";
 import SignedIn from "./signedin";
 import SignedOut from "./signedout";
+import { Suspense } from "react";
 
 export default async function Nav() {
   const routes = [
@@ -16,7 +17,15 @@ export default async function Nav() {
     {route: "/comms", name: "Communications"}
   ]
   
-  const session = await validateRequest()
+  const handleAuth = async () => {
+    const session = await validateRequest()
+    if(session.session){
+      return <SignedIn validatedReq={session} />
+    } else {
+      return <SignedOut />
+    }
+  }
+  
   return (
     <div className="flex justify-between px-[2vw] w-[100%] bg-[#0A304E] items-center text-gray-400" >
       <div>
@@ -25,8 +34,7 @@ export default async function Nav() {
             <Image src={"/usg.jpg"} alt="USG Logo" width={100} height={40}/>
           </Link>
           <div className=" p-2">
-            {session.session && (<SignedIn validatedReq={session} />)}
-            {!session.session && (<SignedOut />)}
+            <Suspense>{handleAuth()} </Suspense>
             </div>
           </div>
       </div>
