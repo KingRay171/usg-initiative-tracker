13a4eb9597a58cd1d249684d4ee0f2fdc01fdd3d
Ray Ikome <raycikome@gmail.com>
2024-04-21 10:06:39 -0400

    removed icon + suspense for division initiative lists

--- app/[division]/page.tsx
+++ app/[division]/page.tsx
@@ -2,6 +2,7 @@ import Init from '../components/init'
 import Link from 'next/link';
 import { InitType } from '@prisma/client';
 import { getInitsForDiv } from '../queries';
+import { Suspense } from 'react';
 
 export default async function Home({params}:{params: {division: string}}) {
   const divisionsDict: {[key: string]: string} = {
@@ -18,13 +19,15 @@ export default async function Home({params}:{params: {division: string}}) {
       return <></>
     }
 
-    const inits = await getInitsForDiv(divisionEnum)
+    
     return (
       <main className="py-8 min-h-screen dark:bg-[#1B1D1E]">
         <div className=" text-center w-full mb-0">
           <h2 className="text-6xl mb-8">{divisionName} Initiatives:</h2>
           <div>
-            {inits.map(e => (<Init key={e.id} info={e} />))}
+            <Suspense >
+              {(await getInitsForDiv(divisionEnum)).map(e => (<Init key={e.id} info={e} />))}
+            </Suspense>
           </div>
           <Link href={`/${params.division}/new`} className='border rounded-lg p-2'>Create an Initiative</Link>
         </div>
