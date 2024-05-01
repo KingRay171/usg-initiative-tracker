f2882771848fab1a23e438cbc8513726b6b16d70
Ray Ikome <raycikome@gmail.com>
2024-05-01 12:31:25 -0400

    png -> svg

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -33,8 +33,8 @@ export default async function Nav() {
     <div className="flex justify-between px-[2vw] w-[100%] bg-[#0A304E] items-center text-gray-400" >
       <div>
         <div className="flex justify-center items-center m-4">
-          <Link href="/">
-            <Image src={"/usg.jpg"} alt="USG Logo" width={100} height={40}/>
+          <Link href="/" className="bg-white">
+            <Image src={"/usg.svg"} alt="USG Logo" width={100} height={40}/>
           </Link>
           <div className=" p-2">
             <Suspense>{handleAuth(session)} </Suspense>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,6 +4,7 @@ import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
+  
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { useFormState, useFormStatus } from 'react-dom';
